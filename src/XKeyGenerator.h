b07147b64d84ddbe4b92477d538781040b5ebc85
Jan-Philip Stecker <janphilip@jp-dev.org>
2015-05-12 22:15:19 +0200

    Add comments to headers

--- src/XKeyGenerator.h
+++ src/XKeyGenerator.h
@@ -1,29 +1,47 @@
-#ifndef XKEY_PASSPHRASE_GENERATOR_H
-#define XKEY_PASSPHRASE_GENERATOR_H
+#pragma once
 
 #include <string>
 #include <vector>
 
 namespace XKey {
 
+static std::initializer_list<char> SpecialCharPool = {'!', '$', '%', '&', '/', '(', ')', '=', '?', '*', '+', '-',
+	'_', '.', ',', ':', ';', '~', '{', '}', '[', ']', '^', '#', '<', '>'
+};
+
+/**
+ * @brief Generator for random passphrases
+ */
 class PassphraseGenerator
 {
 public:
+	/// @brief Allowed characters for a passphrase
 	enum CharacterClass {
+		/// Alphanumeric chars, lowercase
 		CHAR_ALPHA = 1,
+		/// Uppercase characters
 		CHAR_LOWER_UPPERCASE = 2,
+		/// Numeric characters
 		CHAR_NUMERIC = 4,
+		/// Special characters from #SpecialCharPool
 		CHAR_SPECIAL = 8,
 	};
 	
 	PassphraseGenerator ();
 	
+	/// @brief Set the allowed character class pool
 	void setAllowedCharacters (int allowed_classes);
+	
+	/// @brief Disallow a specific character class
 	void disallowCharacterType (CharacterClass c);
 	
 	void setMinLength (int minLen);
 	void setMaxLength (int maxLen);
 	
+	/**
+	 * @brief Generate a new random passphrase from the configured pool
+	 * @param passphrase will contain the generated passphrase
+	 */
 	void generatePassphrase (std::string *passphrase);
 	
 private:
@@ -38,5 +56,3 @@ private:
 };
 
 }
-
-#endif
